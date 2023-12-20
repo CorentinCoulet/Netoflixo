53ef4b65ab170175ab403b24734194ff25b716f6
Coco <coulet.corentin@gmail.com>
2023-12-20 17:30:45 +0100

    correction en cours pour le lancer globale docker

--- api/app.ts
+++ api/app.ts
@@ -1,10 +1,15 @@
-import express, { Express } from 'express';
-import cors from 'cors';
+// import express, { Express } from 'express';
+const express = require('express');
+// import cors from 'cors';
 import authMiddleware from './middleware/authMiddleware';
 import loginRoutes from './routes/loginRoutes';
 import registerRoutes from './routes/registerRoutes';
+const cors = require('cors');
+// const authMiddleware = require('./middleware/authMiddleware');
+// const loginRoutes = require('./routes/loginRoutes');
+// const registerRoutes = require('./routes/registerRoutes');
 
-const app: Express = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +18,7 @@ app.use('/api/secure', authMiddleware);
 app.use('/api/login', loginRoutes);
 app.use('/api/register', registerRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
